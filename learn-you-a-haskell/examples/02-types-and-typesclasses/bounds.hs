c191620ead68cd82f9430f210fb01f5516254c48
deanturpin <git@lvbtn.uk>
2017-01-10 23:04:34 +0000

    types and bounds

---
+++ learn-you-a-haskell/examples/02-types-and-typesclasses/bounds.hs
@@ -0,0 +1,4 @@
+#!/usr/bin/runhaskell
+
+main = do
+  putStrLn $ replicate 3 '#' ++ " Bounds\n"
--- learn-you-a-haskell/examples/02-types-and-typesclasses/types.hs
+++ learn-you-a-haskell/examples/02-types-and-typesclasses/types.hs
@@ -1,15 +1,23 @@
 #!/usr/bin/runhaskell
 
--- all triangles
-range = [1..50]
-triangles = [(a,b,c) | a <- range, b <- range, c <- range]
-triangles' = [(a,b,c) | a <- range, b <- range, c <- range, a^2 * b^2 == c^2]
-triangles'' =
-  [(a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
+factorial n = product [1..n]
+
+-- circumference :: Float -> Float
+circumference :: Double -> Double
+circumference r = 2 * pi * r
+
+-- Functions that have type variables are called polymorphic functions. The type
+-- declaration of head states that it takes a list of any type and returns one
+-- element of that type.
 
 main = do
-  print "triangles"
-  print $ length triangles
-  print $ length triangles'
-  print $ length triangles'
-  print $ triangles''
+  putStrLn $ replicate 3 '#' ++ " Types\n"
+  putStrLn $ show [factorial x | x <- [1..10]]
+  print [product [1..x] | x <- [1..10]]
+  print $ circumference 10
+  print $ compare "hello" "lion"
+  print $ compare 5 4
+  putStrLn $ "hello " ++ show 3.4
+  print $ read "5" + 4
+  print $ read "[1,2,3]" ++ [5]
+  print $ read "1" :: Int
