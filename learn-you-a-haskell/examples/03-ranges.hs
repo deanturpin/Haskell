baaa8268183e6f5631adeff3dd8e214569e3cffa
deanturpin <git@lvbtn.uk>
2017-01-10 17:18:30 +0000

    cycle and repeat

--- learn-you-a-haskell/examples/03-ranges.hs
+++ learn-you-a-haskell/examples/03-ranges.hs
@@ -7,6 +7,16 @@ main = do
   print ['a'..'z']
   print [1,3..10]
   print [10,7..0]
-  print [1.1,2.3..10]
+  print [1.1,2.3..3]
 
-  -- todo: cycle and take
+  -- cycle and take
+  print $ take 10 (cycle [1..3])
+  print $ take 50 (cycle "blah ")
+
+  -- repeat
+  print $ take 5 $ repeat "aa"
+  print $ take 5 $ repeat "a"
+  print $ take 5 $ repeat 'a'
+
+  -- replicate
+  print $ replicate 3 10
