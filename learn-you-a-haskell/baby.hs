e86ff38f586741b9d1df7de6e114d5c652280e7a
deanturpin <git@lvbtn.uk>
2017-01-10 12:21:56 +0000

    add double funcs

--- learn-you-a-haskell/baby.hs
+++ learn-you-a-haskell/baby.hs
@@ -1,3 +1,11 @@
 #!/usr/bin/runhaskell
 
-main = print "hello"
+doubleMe x = x * 2
+doubleUs x y = x*2 + y*2
+doubleUs2 x y = (doubleMe x) + (doubleMe y)
+
+main = do
+  print "double me"
+  print $ doubleMe 5
+  print $ doubleUs 5 6
+  print $ doubleUs2 7 10
