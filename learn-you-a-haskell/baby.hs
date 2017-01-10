60b802708e960a8869c5df4b06bce4991f82e623
deanturpin <git@lvbtn.uk>
2017-01-10 12:27:24 +0000

    conditional doubler

--- learn-you-a-haskell/baby.hs
+++ learn-you-a-haskell/baby.hs
@@ -1,11 +1,23 @@
 #!/usr/bin/runhaskell
 
+-- Basic doublers
 doubleMe x = x * 2
 doubleUs x y = x*2 + y*2
-doubleUs2 x y = (doubleMe x) + (doubleMe y)
+doubleUs2 x y = doubleMe x + doubleMe y
+
+-- Conditiional doublers
+doubleSmallNumber x =
+  if x > 100
+  then x
+  else x*2
 
 main = do
   print "double me"
   print $ doubleMe 5
   print $ doubleUs 5 6
   print $ doubleUs2 7 10
+  print $ doubleUs2 7 10 + doubleMe 4
+  print "double small"
+  print $ doubleSmallNumber 4
+  print "double large"
+  print $ doubleSmallNumber 400
