69b4a07e28a6f2ff31369135fb121bf40b1992f8
deanturpin <git@lvbtn.uk>
2017-01-10 12:52:39 +0000

    lists of lists

--- learn-you-a-haskell/examples/02-lists.hs
+++ learn-you-a-haskell/examples/02-lists.hs
@@ -3,10 +3,25 @@
 -- Basic lists
 lostNumbers = [4,8,15,16,23,42]
 lostNumbers' = ['l','o','s','t']
-badger = ['b','r','i','n','g'] ++ " " ++ ['i','t']
+
+-- Operators
+badger = ['b','r','i','n','g'] ++ " " ++ ['i','t'] -- expensive
+badger' = 'a':['b','r','i','n','g'] -- instant
+badger'' = 1:2:3:4:[]
+
+-- Lists of lists
+animals = [[1,2,3],[1,2,3],[1,2,3],[1,2,3]]
 
 -- Entry point
 main = do
   print lostNumbers
   print lostNumbers'
   print badger
+  print badger'
+  print badger''
+
+  -- Indexing
+  print $ "length"
+  print $ length badger
+  print $ badger !! 1
+  print animals
